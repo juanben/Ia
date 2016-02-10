e3b352d560eed16e26d03900ea7631f3ae382845
afre <a.frex_b@hotmail.com>
2016-02-10 04:02:37 -0500

    Revert "Revert ".""
    This reverts commit a5f40668aa79a35daf0e5aa0ec5e36b8bae85130.

---
+++ src/Comportamientos/ComportamientoAgenteEstacion2.java
@@ -0,0 +1,22 @@
+package Comportamientos;
+
+import Actuadores.Controlador;
+
+import Sensores.Personas;
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
+import jade.lang.acl.ACLMessage;
+
+public class ComportamientoAgenteEstacion2 extends Behaviour{
+	
+	@Override
+	public void action(){
+		
+	}
+
+	@Override
+	public boolean done(){
+		return true;
+	}
+	
+}
